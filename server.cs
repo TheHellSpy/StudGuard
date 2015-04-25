146404013bbd253b3af36f5667d5cf407a812d5c
Sarg3 <blsarg3@gmail.com>
2015-04-25 12:09:52 -0700

    Update server.cs

--- server.cs
+++ server.cs
@@ -1,1 +1,12 @@
-test();
+//StudGuard made by Sarg3 {41072}
+//notify the users that StudGuard is used in a server
+function GameConnection::AutoAdminCheck(%this)
+	{
+		parent::AutoAdminCheck(%this);
+		messageClient(%this,'',"\c3This server is currently running StudGuard");
+
+//executing stuff
+
+exec("./commands/adminc/chelp")
+exec("./commands/adminc/listitems.cs")
+exec("./commands/chat/fixcaps.cs")
